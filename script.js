7c249235a3337b45361853899247fd4e0b5735be
Rohit Nayak <rohit77.rn@gmail.com>
2024-07-22 22:06:35 +0530

    changes

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var timer = 30;
 var score = 0;
 var randomhitNumber = 0;
@@ -53,3 +54,60 @@ makeBubble();
 timerFunction();
 //hitfunction
 hitFunction();
+=======
+let timer = 10;
+let score = 0;
+let randomhitNumber = 0;
+
+let makeBubble = ()=>{
+    let bubblesGalery = "";
+    for(i = 0;i<160;i++){
+        let randomNumber = Math.floor(Math.random()*10);
+        bubblesGalery += `<div class="bubble">${randomNumber}</div>`
+    }
+    document.querySelector(".panelBottom").innerHTML = bubblesGalery;
+}
+
+let timerFunction = ()=>{
+    let timerint = setInterval(() => {
+        if(timer>0){
+            timer--;
+            document.querySelector(".printtimer").textContent = timer;
+        }
+        else{
+            clearInterval(timerint);
+            document.querySelector(".panelBottom").innerHTML = `<h1>Score: ${score} Game Over</h1>`
+        }
+    }, 1000);
+}
+let hitFunction = ()=> {
+    randomhitNumber = Math.floor(Math.random()*10);
+    document.querySelector(".hit").textContent = randomhitNumber;
+}
+
+let scoreFunction = ()=>{
+    score += 10;
+    document.querySelector(".score").textContent = score;
+}
+
+let startThegame = ()=>{
+    let panelBottom = document.querySelector(".panelBottom");
+    panelBottom.addEventListener('click',(bubblesDetail)=>{
+        let clicked = Number(bubblesDetail.target.textContent);
+        if(clicked === randomhitNumber){
+            makeBubble();
+            hitFunction();
+            scoreFunction();
+        }
+    })
+}
+
+//Startgamefunction
+startThegame();
+//makebubbles
+makeBubble();
+//timerfunctio
+timerFunction();
+//hitfunction
+hitFunction();
+>>>>>>> 9f0b0e8 (changes)
